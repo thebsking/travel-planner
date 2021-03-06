ff468c66c496f79a74467078c09f250cb9c1f57b
Brandon King <brandonscottking@gmail.com>
2021-03-06 13:30:11 -0500

    refactored render response sectoin

--- assets/js/script.js
+++ assets/js/script.js
@@ -77,81 +77,20 @@ function getFlights (origin, destination){
 .then(data => {
   console.log(data)
   flightObject = data;
+  
   //render api response data to page
   let flightEL = $('#flightDisplay')
-  flightEL.text(`Cheapest flight is: $${flightObject.Quotes[0].MinPrice}`)
+  flightEL.append(`Cheapest flight is: $${flightObject.Quotes[0].MinPrice}`)
+  secondFlight = flightEL.append('<p>')
+  secondFlight.append(`other options include: $${flightObject.Quotes[1].MinPrice}`)
+  seeMore = secondFlight.append(`<a href="https://www.skyscanner.com/transport/flights/cmha/orlb/210316/210323/?adults=1&adultsv2=1&cabinclass=economy&children=0&childrenv2=&destinationentityid=27542899&inboundaltsenabled=false&infants=0&originentityid=27539848&outboundaltsenabled=false&preferdirects=false&preferflexible=false&ref=home&rtn=1">See More</a>`)
 })
 .catch(err => {
 	console.error(err);
 });
 }
 
-//build maps api calls https://developers.google.com/maps/documentation/javascript/overview
-function getAttractions(destination){
-  function initMap() {
-    // Create the map.
-    const pyrmont = { lat: 40, lng: -83 };
-    const map = new google.maps.Map(document.getElementById("map"), {
-      center: pyrmont,
-      zoom: 17,
-      mapId: "8d193001f940fde3",
-    });
-    // Create the places service.
-    const service = new google.maps.places.PlacesService(map);
-    let getNextPage;
-    const moreButton = document.getElementById("more");
-  
-    moreButton.onclick = function () {
-      moreButton.disabled = true;
-  
-      if (getNextPage) {
-        getNextPage();
-      }
-    };
-    // Perform a nearby search.
-    service.nearbySearch(
-      { location: pyrmont, radius: 1000, type: "attractions" },
-      (results, status, pagination) => {
-        if (status !== "OK" || !results) return;
-        addPlaces(results, map);
-        moreButton.disabled = !pagination || !pagination.hasNextPage;
-  
-        if (pagination && pagination.hasNextPage) {
-          getNextPage = () => {
-            // Note: nextPage will call the same handler function as the initial call
-            pagination.nextPage();
-          };
-        }
-      }
-    );
-  }
-  
-  function addPlaces(places, map) {
-    const placesList = document.getElementById("places");
-  
-    for (const place of places) {
-      if (place.geometry && place.geometry.location) {
-        const image = {
-          url: place.icon,
-          size: new google.maps.Size(71, 71),
-          origin: new google.maps.Point(0, 0),
-          anchor: new google.maps.Point(17, 34),
-          scaledSize: new google.maps.Size(25, 25),
-        };
-        new google.maps.Marker({
-          map,
-          icon: image,
-          title: place.name,
-          position: place.geometry.location,
-        });
-        const li = document.createElement("li");
-        li.textContent = place.name;
-        placesList.appendChild(li);
-        li.addEventListener("click", () => {
-          map.setCenter(place.geometry.location);
-        });
-      }
-    }
-  }
-}
-
+//add click event for submission
+$('.flights').on('click', function(){
+  getCities($('#depCity').val(), $('#arrCity').val());
+})
