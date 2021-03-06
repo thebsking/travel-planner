1001e0527b034f613f646e03262ac64b8158a217
Kyle Euman <euman_kyle@yahoo.com>
2021-03-06 11:47:17 -0500

    Added Search for city button

--- assets/js/script.js
+++ assets/js/script.js
@@ -74,44 +74,6 @@ function getFlights (origin, destination){
 });
 }
 
-let apiKey = "3ac0d8db34de82819d13a9167239acc1";
-let searchBtn = $(".searchBtn");
-let searchInput = $(".searchInput");
-
-
-let cityNameEl = $(".CityName");
-let currentDateEl = $(".currentdate");
-let weatherIconEl = $(".WeatherIcon");
-let searchHistoryEl = $(".history");
-
-
-let tempEl = $(".temp");
-let humidityEl = $(".humidity");
-let windSpeedEl = $(".windspeed");
-let uvIndexEl = $(".UVindex");
-
-var today = new Date();
-let dd = String(today.getDate()).padStart(2, '0');
-let mm = String(today.getMonth() + 1).padStart(2, '0');
-let yyyy = today.getFullYear();
-var today = mm + '/' + dd + '/' +yyyy;
-
-if (JSON.parse(localStorage.getItem)('searchHistory'))  null 
-  console.log('searchHistory not found')
-
- (console.log('searchHistory loaded into searchHistoryArr'));
-  renderSearchHistory();
-
-
-searchBtn.on('click', function(e) {
-  e.preventDefault();
-  if (searchInput.val() === ''){
-    alert('You Must Pick A City');
-    return;
-  }
-  console.log('clicked button')
-  getWeather(searchInput.val());
-}
 
 //build maps api calls https://developers.google.com/maps/documentation/javascript/overview
 
@@ -123,4 +85,4 @@ $('.flights').on('click', function(){
 
 
 //render api response data to page 
-)
+
